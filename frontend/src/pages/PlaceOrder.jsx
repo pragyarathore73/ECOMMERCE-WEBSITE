c620a01798fa729058910efa362dfb4129a4abcf
Pragya Rathore <pragyarathore73@gmail.com>
2026-08-18 18:54:33 +0530

    Refactor PlaceOrder component for payment methods

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -95,15 +95,15 @@ const PlaceOrder = () => {
                     }
                     break;
 
-                case 'stripe':
-                    const responseStripe = await axios.post(backendUrl + '/api/order/stripe',orderData,{headers:{token}})
-                    if (responseStripe.data.success) {
-                        const {session_url} = responseStripe.data
-                        window.location.replace(session_url)
-                    } else {
-                        toast.error(responseStripe.data.message)
-                    }
-                    break;
+                // case 'stripe':
+                //     const responseStripe = await axios.post(backendUrl + '/api/order/stripe',orderData,{headers:{token}})
+                //     if (responseStripe.data.success) {
+                //         const {session_url} = responseStripe.data
+                //         window.location.replace(session_url)
+                //     } else {
+                //         toast.error(responseStripe.data.message)
+                //     }
+                //     break;
 
                 case 'razorpay':
 
